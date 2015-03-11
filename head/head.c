9ec72ee27cc1eb7437f0101487f51a120fdc38ea
skatsuta <katsuta_soshi@cyberagent.co.jp>
2015-03-12 02:34:08 +0900

    head: take options

--- head/head.c
+++ head/head.c
@@ -1,26 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define _GNU_SOURCE
+#include <getopt.h>
 
 static void do_head(FILE *f, long nlines);
 
-int main(int argc, char *argv[]) {
-	long nlines;
+#define DEFAULT_N_LINES 10
 
-	if (argc < 2) {
-		fprintf(stderr, "Usage: %s n [file1 file2 ...]\n", argv[0]);
-		exit(1);
+static struct option longopts[] = {
+	{"lines", required_argument, NULL, 'n'},
+	{"help",  no_argument,       NULL, 'h'},
+	{0, 0, 0, 0}
+};
+
+int main(int argc, char *argv[]) {
+	int opt;
+	long nlines = DEFAULT_N_LINES;
+
+	// process options
+	while ((opt = getopt_long(argc, argv, "n:", longopts, NULL)) != -1) {
+		switch (opt) {
+			case 'n':
+				nlines = atoi(optarg);
+				break;
+			case 'h':
+				fprintf(stdout, "Usage: %s [-n LINES] [file ...]\n", argv[0]);
+				exit(0);
+			case '?':
+				fprintf(stderr, "Usage: %s [-n LINES] [file ...]\n", argv[0]);
+				exit(1);
+		}
 	}
 
-	nlines = atol(argv[1]);
-
-	if (argc == 2) {
+	if (optind == argc) {
 		do_head(stdin, nlines);
 		exit(0);
 	}
 
 	int i;
 
-	for (i = 2; i < argc; i++) {
+	for (i = optind; i < argc; i++) {
 		FILE *f;
 
 		f = fopen(argv[i], "r");
