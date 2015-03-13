cd3e2c00f1f45246f7ff6e2460b2788723550b6c
skatsuta <katsuta_soshi@cyberagent.co.jp>
2015-03-14 02:24:01 +0900

    cat2: print end and tab if the options is given

--- cat/cat2.go
+++ cat/cat2.go
@@ -8,16 +8,53 @@ import (
 	"os"
 )
 
-func main() {
+type replacer interface {
+	replace(b byte) string
+}
+
+type disp struct {
+	end bool
+	tab bool
+}
+
+func (d *disp) replace(b byte) string {
+	if b == '\n' && d.end {
+		return "$\n"
+	}
+
+	if b == '\t' && d.tab {
+		return "> "
+	}
+
+	return string(b)
+}
+
+func newDisp() *disp {
+	d := new(disp)
+
+	// define flags
+	flag.BoolVar(&d.end, "e", false, "display a dollar sign (`$`) at the end of each line")
+	flag.BoolVar(&d.tab, "t", false, "display tab characters as `> `")
+	flag.Parse()
 	flag.Usage = usage
 
-	// process flags
-	flag.Bool("e", false, "display a dollar sign (`$`) at the end of each line")
-	flag.Bool("t", false, "display tab characters as `	`")
-	flag.Parse()
+	return d
+}
 
-	for i := 1; i < len(os.Args); i++ {
-		fp, err := os.Open(os.Args[i])
+func main() {
+	d := newDisp()
+
+	files := flag.Args()
+
+	if len(files) < 1 {
+		if e := doCat(os.Stdin, d); e != nil {
+			die(e)
+		}
+		return
+	}
+
+	for _, file := range files {
+		fp, err := os.Open(file)
 		if err != nil {
 			die(err)
 		}
@@ -27,28 +64,43 @@ func main() {
 			}
 		}()
 
-		r := bufio.NewReader(fp)
-		w := bufio.NewWriter(os.Stdout)
-
-		for {
-			c, err := r.ReadByte()
-			if err == io.EOF {
-				break
-			}
-
-			if e := w.WriteByte(c); e != nil {
-				die(e)
-			}
-		}
-
-		if e := w.Flush(); e != nil {
+		if e := doCat(fp, d); e != nil {
 			die(e)
 		}
 	}
+}
 
+func doCat(fp *os.File, rpl replacer) error {
+	r := bufio.NewReader(fp)
+	w := bufio.NewWriter(os.Stdout)
+
+	for {
+		c, err := r.ReadByte()
+		if err == io.EOF {
+			break
+		}
+
+		s := rpl.replace(c)
+
+		if _, e := w.WriteString(s); e != nil {
+			return e
+		}
+
+		if c == '\n' {
+			if e := w.Flush(); e != nil {
+				return e
+			}
+		}
+	}
+
+	return w.Flush()
 }
 
 func die(err error) {
+	if err == nil {
+		return
+	}
+
 	fmt.Fprintf(os.Stderr, "%v\n", err)
 	os.Exit(1)
 }
