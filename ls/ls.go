07359bf8146fc3ee30a47da2663973b73af1179f
skatsuta <katsuta_soshi@cyberagent.co.jp>
2015-03-15 01:03:04 +0900

    ls.go: add flags -A and -R

--- ls/ls.go
+++ ls/ls.go
@@ -1,22 +1,48 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strings"
 )
 
+const (
+	unixHiddenFilePrefix = "."
+	currentDir           = "."
+)
+
+type option struct {
+	all bool
+	rec bool
+}
+
+func newOption() option {
+	opt := option{}
+
+	flag.BoolVar(&opt.all, "A", false, "list all entries except for . and ..")
+	flag.BoolVar(&opt.rec, "R", false, "recursively list subdirectories encountered")
+
+	flag.Parse()
+
+	return opt
+}
+
 func main() {
-	if len(os.Args) < 2 {
-		doLs(".")
+	opt := newOption()
+
+	paths := flag.Args()
+	if len(paths) == 0 {
+		paths = []string{currentDir}
 	}
 
-	for _, path := range os.Args {
-		doLs(path)
+	for _, path := range paths {
+		doLs(path, opt)
 	}
 }
 
-func doLs(path string) {
+func doLs(path string, opt option) {
 	fis, err := ioutil.ReadDir(path)
 	if err != nil {
 		perror(err)
@@ -24,8 +50,27 @@ func doLs(path string) {
 	}
 
 	for _, fi := range fis {
-		fmt.Printf("%s\n", fi.Name())
+		name := fi.Name()
+
+		// skip the following process if not to display hidden files
+		skip := !opt.all && isHidden(name)
+		if skip {
+			continue
+		}
+
+		fmt.Println(name)
+
+		// run recursively
+		isRec := opt.rec && fi.IsDir()
+		if isRec {
+			name = path + "/" + name
+			doLs(name, opt)
+		}
 	}
+}
+
+func isHidden(path string) bool {
+	return strings.HasPrefix(path, unixHiddenFilePrefix)
 }
 
 func perror(err error) {
